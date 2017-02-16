4cbea15b15ebf06bd0492df79dac0f78552e6deb
Vlad Silviu Farcas <vladsilviufarcas@gmail.com>
2017-02-16 19:43:23 +0200

    test

--- pizzavraa.pl
+++ pizzavraa.pl
@@ -5,13 +5,35 @@
 :- use_module(library(chr)).		% Constraint Handling Rules
 :- use_module(library(when)).		% Coroutining
 
-small(6, 7, 1, 5, [[T, M, M, M, T, T, T],
-[M, M, M, M, T, M, M],
-[T, T, M, T, T, M, T],
-[T, M, M, T, M, M, M],
-[T, T, T, T, T, T, M],
-[T, T, T, T, T, T, M]]).
+small(6, 7, 1, 5, [[t, m, m, m, t, t, t],
+[m, m, m, m, t, m, m],
+[t, t, m, t, t, m, t],
+[t, m, m, t, m, m, m],
+[t, t, t, t, t, t, m],
+[t, t, t, t, t, t, m]]).
 
-solve(Slices, Solution):- small(R, C, L, H, Pizza)
-    %tbc
-    .
+row(M, N, Row) :-
+    nth1(N, M, Row).
+
+first_column([], [], []).
+first_column([[]|_], [], []).
+first_column([[I|Is]|Rs], [I|Col], [Is|Rest]) :-
+    first_column(Rs, Col, Rest).
+
+transpose([[]|_], []) :- !.
+transpose([[I|Is]|Rs], [Col|MT]) :-
+    first_column([[I|Is]|Rs], Col, [Is|NRs]),
+    transpose([Is|NRs], MT).
+
+column(M, N, Col) :-
+    transpose(M, MT),
+    row(MT, N, Col).
+
+at(Row, Col, I):-
+    small(_, _, _, _, Pizza), 
+    row(Pizza, Row, IPartial),
+    column([IPartial], Col, [I]).
+
+solve(Slices, Solution):- small(R, C, L, H, Pizza),
+    at(2, 5, Solution),
+    Slices #= 1.
